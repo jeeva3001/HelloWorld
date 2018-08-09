284b8dad10a5f3c1301c3fc27979ca3ac20c90d8
jeevanantham <jeevanantham.natarajan@itcinfotech.com>
2018-08-09 07:09:29 +0000

    only restored helloworld.java from commit dd3f7ae

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,5 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-	//Print  "welcome to git//" in the terminal window.
-      System.out.println("Welcome to git!!"):
    }
 }
